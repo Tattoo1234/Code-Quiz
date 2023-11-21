44a0e7b83a7777f3831b2496354d2998cdf93a46
Tattoo1234 <147221184+Tattoo1234@users.noreply.github.com>
2023-11-21 15:38:34 -0800

    started JS

---
+++ script.js
@@ -0,0 +1,8 @@
+var pageOneEl = document.querySelector("#firstPage")
+
+document.querySelector("#startQuiz")
+    .addEventListener("click", )
+
+function switchPages(){
+    if(******="hide")
+}    
