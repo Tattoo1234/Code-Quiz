7ebcdfb98e5947aad22619d7fe0545b4c7aa74ed
Tattoo1234 <147221184+Tattoo1234@users.noreply.github.com>
2023-11-21 16:10:00 -0800

    added functions, and var in js

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 var pageOneEl = document.querySelector("#firstPage")
+var pageTwoEl = document.querySelector("#secondPage")
+var pageThreeEl = document.querySelector("#thirdPage")
+var pageFourEl = document.querySelector("#fourthPage")
+
 
 document.querySelector("#startQuiz")
-    .addEventListener("click", )
+    .addEventListener("click",switchPages)
 
 function switchPages(){
-    if(******="hide")
+    pageOneEl.classList.toggle("hide");
+    pageTwoEl.classList.toggle("hide");
 }    
+
+var strings = pageTwoEl.children[1].children[0]
+var booleans = pageTwoEl.children[1].children[1]
+var alerts = pageTwoEl.children[1].children[2]
+var numbers = pageTwoEl.children[1].children[3]
