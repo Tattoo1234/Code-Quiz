ec4d0fd8588dd0e84f3cf53e1dbf7551be45f3f3
Tattoo1234 <147221184+Tattoo1234@users.noreply.github.com>
2023-11-25 15:24:39 -0800

    started 2nd set of answers

--- script.js
+++ script.js
@@ -6,30 +6,29 @@ var timerEl = document.getElementById('timer');
 
 
 function countdown() {
-    var timeLeft = 60;
-  
+    var timeLeft = 10;
+
     // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
     var timeInterval = setInterval(function () {
-      // As long as the `timeLeft` is greater than 1
-      if (timeLeft > 1) {
-        // Set the `textContent` of `timerEl` to show the remaining seconds
-        timerEl.textContent = timeLeft + ' seconds remaining';
-        // Decrement `timeLeft` by 1
-        timeLeft--;
-      } else if (timeLeft === 1) {
-        // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-        timerEl.textContent = timeLeft + ' second remaining';
-        timeLeft--;
-      } else {
-        // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-        timerEl.textContent = '';
-        // Use `clearInterval()` to stop the timer
-        clearInterval(timeInterval);
-        // Call the `displayMessage()` function
-        displayMessage();
-      }
+        // As long as the `timeLeft` is greater than 1
+        if (timeLeft > 1) {
+            // Set the `textContent` of `timerEl` to show the remaining seconds
+            timerEl.textContent = timeLeft + ' seconds remaining';
+            // Decrement `timeLeft` by 1
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+            timerEl.textContent = timeLeft + ' second remaining';
+            timeLeft--;
+        } else {
+            // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+            timerEl.textContent = 'Times up!';
+            // Use `clearInterval()` to stop the timer
+            clearInterval(timeInterval);
+
+        }
     }, 1000);
-  }
+}
 
 
 document.querySelector("#startQuiz")
@@ -55,14 +54,18 @@ var correctAnswer = answerThree
 
 for (let i = 0; i < answerArray.length; i++)
     answerArray[i].addEventListener("click", function (event) {
-       
+
         if (correctAnswer === event.target) {
             console.log("correct")
         }
-             else {
+        else {
             console.log("incorrect")
         }
     })
 
+  answerOne.textContent = "commas";
+  answerTwo.textContent = "curly brakets"
+  answerThree.textContent = "quotes"
+  answerFour.textContent = "parenthesis"
 
 
