55388f0e676bba50efdd142c224b69806cc65119
Tattoo1234 <147221184+Tattoo1234@users.noreply.github.com>
2023-11-25 14:27:37 -0800

    worked with for loop and conditional

--- script.js
+++ script.js
@@ -5,14 +5,31 @@ var pageFourEl = document.querySelector("#fourthPage")
 
 
 document.querySelector("#startQuiz")
-    .addEventListener("click",switchPages)
+    .addEventListener("click", switchPages)
 
-function switchPages(){
+function switchPages() {
     pageOneEl.classList.toggle("hide");
     pageTwoEl.classList.toggle("hide");
-}    
+}
 
-var strings = pageTwoEl.children[1].children[0]
-var booleans = pageTwoEl.children[1].children[1]
-var alerts = pageTwoEl.children[1].children[2]
-var numbers = pageTwoEl.children[1].children[3]
+var answerOne = pageTwoEl.children[1].children[0]
+var answerTwo = pageTwoEl.children[1].children[1]
+var answerThree = pageTwoEl.children[1].children[2]
+var answerFour = pageTwoEl.children[1].children[3]
+
+var firstAnswers = [answerOne, answerTwo, answerThree, answerFour]
+
+
+
+for (let i = 0; i < firstAnswers.length; i++)
+    firstAnswers[i].addEventListener("click", function () {
+        console.log(firstAnswers[i])
+    })
+
+if (answerThree) {
+    console.log("correct")
+}
+     else {
+    console.log("incorrect")
+}
+
